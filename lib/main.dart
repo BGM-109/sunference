8b7e2b5c2405d5ba38d83dc0cd66b62e98eeefb8
“vividxxxxx” <vividxxxx@gmail.com>
2021-10-05 17:39:36 +0900

    create future builder, display data

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,13 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   Future getData() async {
-    var url = "https://raw.githubusercontent.com/junsuk5/mock_json/main/conferences.json";
+    var url =
+        "https://raw.githubusercontent.com/junsuk5/mock_json/main/conferences.json";
     var uri = Uri.parse(url);
     var response = await http.get(uri);
 
     if (response.statusCode == 200) {
-      var data = convert.jsonDecode(response.body);
+      List data = convert.jsonDecode(response.body);
       return data;
     } else {
       print('Request failed with status: ${response.statusCode}.');
@@ -31,9 +32,26 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text("Conference"),
         ),
-        body: const Center(
-          child: Text("Lets build app"),
-        ),
+        body: FutureBuilder(
+            future: getData(),
+            builder: (context, AsyncSnapshot snapshot) {
+              if (snapshot.hasData) {
+                return ListView.separated(
+                    separatorBuilder: (BuildContext context, int index) => const Divider(height: 1,color: Colors.white,),
+                    itemCount: snapshot.data?.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text(snapshot.data[index]["name"]),
+                        subtitle: Text(snapshot.data[index]["location"]),
+                        onTap: (){},
+                      );
+                    });
+              } else if (snapshot.hasError) {
+                return const Center(child: Text("Error!!!!!"));
+              } else {
+                return const CircularProgressIndicator();
+              }
+            }),
         floatingActionButton: FloatingActionButton(
           child: const Icon(Icons.add),
           onPressed: () {
